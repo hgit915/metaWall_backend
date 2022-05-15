b8cda9e8af5010184c390f3bac927a5abedd34db
wendy03 <wendysu03@gmail.com>
2022-05-16 01:17:57 +0800

    feat: add user sign up and sign in

---
+++ controllers/users.js
@@ -0,0 +1,55 @@
+const bcrypt = require('bcryptjs')
+const validator = require('validator')
+const User = require('../models/users')
+const appError = require('../service/appError')
+const handleErrorAsync = require('../service/handleErrorAsync')
+const { generateSendJWT } = require('../service/auth')
+
+const users = {
+  signUp: handleErrorAsync(async (req, res, next) => {
+    let { email, password, name } = req.body
+    const user = await User.findOne({ email })
+    if (user) {
+      return appError('400', '帳號已被註冊，請替換新的 Email！', next)
+    }
+    // 內容不可為空
+    if (!email || !password || !name) {
+      return appError('400', '欄位未填寫正確！', next)
+    }
+    // 暱稱 2 碼以上
+    if (!validator.isLength(name, { min: 2 })) {
+      return appError('400', '暱稱至少 2 個字元以上', next)
+    }
+    // 密碼 8 碼以上
+    if (!validator.isLength(password, { min: 8 })) {
+      return appError('400', '密碼需至少 8 碼以上，並中英混合', next)
+    }
+    // 是否為 Email
+    if (!validator.isEmail(email)) {
+      return appError('400', 'Email 格式不正確', next)
+    }
+
+    // 加密密碼
+    password = await bcrypt.hash(req.body.password, 12)
+    const newUser = await User.create({
+      email,
+      password,
+      name
+    })
+    generateSendJWT(newUser, 201, res)
+  }),
+  signIn: handleErrorAsync(async (req, res, next) => {
+    const { email, password } = req.body
+    if (!email || !password) {
+      return appError(400, '帳號密碼不可為空', next)
+    }
+    const user = await User.findOne({ email }).select('+password')
+    const auth = await bcrypt.compare(password, user.password)
+    if (!auth || !user) {
+      return appError(400, '帳號或密碼錯誤，請重新輸入！', next)
+    }
+    generateSendJWT(user, 200, res)
+  })
+}
+
+module.exports = users
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const UsersControllers = require('../controllers/users')
+
+router.post('/sign_up', UsersControllers.signUp)
+router.post('/sign_in', UsersControllers.signIn)
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
