748f2c4a52d704a138fac04b05e4b17e8035a7ff
wendy03 <wendysu03@gmail.com>
2022-05-16 01:20:34 +0800

    feat: add facebook login

---
+++ config/passport.js
@@ -0,0 +1,57 @@
+const FacebookStrategy = require('passport-facebook').Strategy
+const GoogleStrategy = require('passport-google-oauth2').Strategy
+const bcrypt = require('bcryptjs')
+const User = require('../models/users')
+
+module.exports = (passport) => {
+  // Facebook 登入
+  passport.use(
+    new FacebookStrategy(
+      {
+        clientID: process.env.FACEBOOK_ID,
+        clientSecret: process.env.FACEBOOK_SECRET,
+        callbackURL: process.env.FACEBOOK_CALLBACK,
+        profileFields: ['email', 'displayName']
+      },
+      (accessToken, refreshToken, profile, done) => {
+        // find and create user
+        User.findOne({
+          email: profile._json.email
+        }).then((user) => {
+          // 如果 email 不存在就建立新的使用者
+          if (!user) {
+            const randomPassword = Math.random().toString(36).slice(-8)
+            bcrypt.genSalt(12, (_err, salt) =>
+              bcrypt.hash(randomPassword, salt, (_err, hash) => {
+                const newUser = User({
+                  name: profile._json.name,
+                  email: profile._json.email,
+                  password: hash
+                })
+                newUser
+                  .save()
+                  .then((user) => {
+                    return done(null, user)
+                  })
+                  .catch((err) => {
+                    console.log(err)
+                  })
+              })
+            )
+          } else {
+            return done(null, user)
+          }
+        })
+      }
+    )
+  )
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+  })
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+      done(err, user)
+    })
+  })
+}
