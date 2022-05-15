651ba8cc6a51e61938e31443e2c6333ed7158bab
wendy03 <wendysu03@gmail.com>
2022-05-16 01:21:11 +0800

    feat: add google login

--- config/passport.js
+++ config/passport.js
@@ -46,6 +46,48 @@ module.exports = (passport) => {
     )
   )
 
+  // Google 登入
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: process.env.GOOGL_CALLBACK,
+        profileFields: ['email', 'displayName']
+      },
+      (accessToken, refreshToken, profile, done) => {
+        // find and create user
+        User.findOne({
+          email: profile._json.email
+        }).then((user) => {
+          // 如果 email 不存在就建立新的使用者
+          if (!user) {
+            const randomPassword = Math.random().toString(36).slice(-8)
+            bcrypt.genSalt(12, (_err, salt) =>
+              bcrypt.hash(randomPassword, salt, (_err, hash) => {
+                const newUser = User({
+                  name: profile._json.name,
+                  email: profile._json.email,
+                  password: hash
+                })
+                newUser
+                  .save()
+                  .then((user) => {
+                    return done(null, user)
+                  })
+                  .catch((err) => {
+                    console.log(err)
+                  })
+              })
+            )
+          } else {
+            return done(null, user)
+          }
+        })
+      }
+    )
+  )
+
   passport.serializeUser((user, done) => {
     done(null, user.id)
   })
--- routes/auths.js
+++ routes/auths.js
@@ -13,4 +13,18 @@ router.get(
   }
 )
 
+router.get(
+  '/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+)
+
+router.get(
+  '/google/redirect',
+  passport.authenticate('google', { failureRedirect: '/sign_in' }),
+  (req, res) => {
+    res.redirect('/posts')
+  }
+)
 module.exports = router
