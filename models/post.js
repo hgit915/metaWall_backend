c03f7f65d3e387e5922fdc7f3d34b237be18c4d0
Qianyi <qianchen@eztravel.com.tw>
2022-05-15 12:19:19 +0800

    feat : add server.js

---
+++ models/post.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema(
+  {
+    userName: { type: String, required: [true, '發文者名稱為必填'] },
+    userPhoto: { type: String, required: [true, '發文者照片為必填'] },
+    imageUrl: { type: String },
+    content: { type: String },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+module.exports = mongoose.model('Post', schema);
