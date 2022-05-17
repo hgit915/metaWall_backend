f7e5795eae24d7ebcd320dc4a04f70721b616aa1
Qianyi <hgit915@gmail.com>
2022-05-18 03:32:36 +0800

    feat : edit message / post

--- controllers/message.js
+++ controllers/message.js
@@ -1,8 +1,22 @@
 const validator = require('validator')
 const Message = require('../models/message')
+const appError = require('../service/appError')
+const handleErrorAsync = require('../service/handleErrorAsync')
+const successHandler = require('../service/handleSuccess')
 
-const messages = {
+async function editMessage (req, res, next) {
+  const { content } = req.body
+  const id = req.params.postId
 
+  if (!content) {
+    return appError(400, '留言內容不可為空', next)
+  }
+
+  Message.findByIdAndUpdate(id, {
+    content
+  }).then(() => successHandler(res, 'update messages success', Message.findById(id)))
 }
 
-module.exports = messages
+module.exports = {
+  editMessage: handleErrorAsync(editMessage)
+}
