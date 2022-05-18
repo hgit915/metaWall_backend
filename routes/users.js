95803d848ef3dbe179682e6f4301f8e470ac7907
wins <win4709hi@gmail.com>
2022-05-18 22:55:04 +0800

    chore: add get and delete user api, easy to test

--- routes/users.js
+++ routes/users.js
@@ -5,4 +5,8 @@ const UsersControllers = require('../controllers/users')
 router.post('/sign_up', UsersControllers.signUp)
 router.post('/sign_in', UsersControllers.signIn)
 
+// 測試方便用
+router.get('/', UsersControllers.getAllUser)
+router.delete('/', UsersControllers.deleteUser)
+
 module.exports = router
