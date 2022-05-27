d5de2095c0676d80c279327962af3e56105ee053
wendy03 <wendysu03@gmail.com>
2022-05-27 22:42:44 +0800

    feat: fix thirdparty-login

--- routes/auths.js
+++ routes/auths.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+const { generateUrlJWT } = require('../service/auth')
+
 router.get(
   '/facebook',
   passport.authenticate('facebook', { scope: ['email', 'public_profile'] })
@@ -9,7 +11,8 @@ router.get(
   '/facebook/callback',
   passport.authenticate('facebook', { failureRedirect: '/sign_in' }),
   (req, res) => {
-    res.redirect('/posts')
+    const user = req.user
+    generateUrlJWT(res, user)
   }
 )
 
@@ -24,7 +27,9 @@ router.get(
   '/google/redirect',
   passport.authenticate('google', { failureRedirect: '/sign_in' }),
   (req, res) => {
-    res.redirect('/posts')
+    const user = req.user
+    generateUrlJWT(res, user)
   }
 )
+
 module.exports = router
