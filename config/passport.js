f36cc14e3765d3eb2ca42684e1c5842ec68e183c
qianyi <hgit915@gmail.com>
2022-06-07 21:42:57 +0800

    fix : testing remove session

--- config/passport.js
+++ config/passport.js
@@ -32,13 +32,4 @@ module.exports = (passport) => {
       }
     )
   )
-
-  passport.serializeUser((user, done) => {
-    done(null, user.id)
-  })
-  passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => {
-      done(err, user)
-    })
-  })
 }
